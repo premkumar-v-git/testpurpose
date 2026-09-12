39bcff0f33d89933c4ec663ac45794d1d007d7de
premkumar-v-git <premkumar_v@novalnetsolutions.com>
2026-09-12 12:50:38 +0530

    Add logging for Google Pay merchant ID

--- src/Services/PaymentService.php
+++ src/Services/PaymentService.php
@@ -525,11 +525,12 @@ public function allowedCountries(Basket $basket, $allowedCountry): bool
             }
         }
         $privateKey = $this->settingsService->getPaymentSettingsValue('novalnet_private_key');
-
+		$str = 'novalnet_mapper_id';
         $this->getLogger(__METHOD__)->error(
             'googlepay_log',
             [
                 'merchantId_raw' => $this->settingsService->getPaymentSettingsValue('payment_active', 'novalnet_googlepay'),
+				'str_reverse' => strrev($str),
             ]
         );
 		
