4f0fcb70ed8af9bcb6efb018c6fac9f6b6af765d
premkumar-v-git <premkumar_v@novalnetsolutions.com>
2026-09-12 13:03:19 +0530

    Update logging in PaymentService for string reversal
    Replace commented-out logging with new error logging for string reversal.

--- src/Services/PaymentService.php
+++ src/Services/PaymentService.php
@@ -525,20 +525,13 @@ public function allowedCountries(Basket $basket, $allowedCountry): bool
             }
         }
         $privateKey = $this->settingsService->getPaymentSettingsValue('novalnet_private_key');
-
-// 		$str = 'novalnet_mapper_id';
-
-// $this->getLogger(__METHOD__)->error(
-//     'googlepay_log',
-//     [
-//         'merchantId_raw' => $this->settingsService->getPaymentSettingsValue(
-//             'payment_active',
-//             'novalnet_googlepay'
-//         ),
-//         'str_reverse' => strrev($str),
-//     ]
-// );
 		
+		$this->getLogger(__METHOD__)->error(
+			'str_rev_log',
+			[
+				'str_rev' => strrev('hello')
+			]
+		);
         $paymentResponseData = $this->paymentHelper->executeCurl($paymentRequestData['paymentRequestData'], $paymentRequestData['paymentUrl'], $privateKey);
         $isPaymentSuccess = isset($paymentResponseData['result']['status']) && $paymentResponseData['result']['status'] == 'SUCCESS';
         // Do redirect if the redirect URL is present
